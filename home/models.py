30cb403c45ae7f89941f409311ff38caf6517bad
niklas eriksson <niklaseriksson32@hotmail.com>
2024-05-19 13:17:34 +0000

    add post model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.modles import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+# shows recipe post if drafted or published
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+# models for create recipe post
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipe_post")
+    content = models.TextField()
+    image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name="recipe_likes", blank=True)
+
+  # presents wich order the recipes was creaed on
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def likes(self):
+        return self.likes.count()
+
+
