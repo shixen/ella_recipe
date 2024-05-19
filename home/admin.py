bced54e4bfb1b691ec4eae72f7bfd52136e5b2b8
niklas eriksson <niklaseriksson32@hotmail.com>
2024-05-19 14:02:29 +0000

    add summernote in admin

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Post)
+@admin.register(Post)
+# includes the summernote in the context field of posting a recipe
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
+
+
