8e2066fd133a970400d66adf635418f7ae108634
niklas eriksson <niklaseriksson32@hotmail.com>
2024-05-20 20:26:19 +0000

    deployt settings

--- recipe/settings.py
+++ recipe/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-shixen-ellarecipe-qtbj6ge2c2f.ws-eu111.gitpod.io','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-shixen-ellarecipe-qtbj6ge2c2f.ws-eu114.gitpod.io','.herokuapp.com']
 
 
 # Application definition
