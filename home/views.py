d284a12822e85b234caa908e4a1541dbae320e1d
niklas eriksson <niklaseriksson32@hotmail.com>
2024-05-24 10:11:12 +0000

    add ordering of comments

--- home/views.py
+++ home/views.py
@@ -11,10 +11,10 @@ class PostList(generic.ListView):
 # gets specific recipe to read larger post
 class recipeDetails(View):
 
-    def get(self, request, slug):
+    def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True)
+        comments = post.comments.filter(approved=True). order_by('created_on')
 
   # renders view to html page
         return render(
