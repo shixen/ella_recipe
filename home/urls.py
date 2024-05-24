90598c1a6525feb722fa9bc51ce951e640077d52
niklas eriksson <niklaseriksson32@hotmail.com>
2024-05-24 10:10:56 +0000

    comments

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,7 @@
 from . import views
 from django.urls import path
 
-# imports urls paths
+# urls paths
 urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
     path('<slug:slug>/', views.recipeDetails.as_view(), name='recipe_details'),
