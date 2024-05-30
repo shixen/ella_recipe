9ed8da9248e51c3a74e177a3fbee283b13396e58
niklas eriksson <niklaseriksson32@hotmail.com>
2024-05-30 16:59:33 +0000

    add f string return on forms

--- forms/models.py
+++ forms/models.py
@@ -6,3 +6,5 @@ class ContactForm(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField()
     description = models.TextField(blank=True)
+    def __str__(self) :
+        return f'Message from {self.name}'
