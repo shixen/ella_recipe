21a4ff1827c78cffe360548499d03fd9527b404e
niklas eriksson <niklaseriksson32@hotmail.com>
2024-05-30 20:16:08 +0000

    add return render and redirect to views

--- forms/views.py
+++ forms/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
@@ -8,11 +8,11 @@ from .models import ContactForm
 # gets info from form and saves in database
 def ContactForm(request):
     if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        description = request.POST.get('description')
-        query=ContactForm(name=name, email=email, description=description)
+        fname = request.POST.get('name')
+        femail = request.POST.get('email')
+        description = request.POST.get('desc')
+        query=ContactForm(name=fname, email=femail, description=desc)
         query.save()
         messages.info(request, 'Thanks for ordering!')
         return redirect('/contact')
-    return(render, request, 'contact.html')
+    return render(request,'contact.html')
