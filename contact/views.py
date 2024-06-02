3b88734b6aa6de3ae5f3067bfe9eac814ba560d6
niklas eriksson <niklaseriksson32@hotmail.com>
2024-06-02 18:30:19 +0000

    add views for contact form

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Contact
 
-# Create your views here.
+def contact(request):
+    if request.method == 'POST':
+        fname = request.POST.get('name')
+        femail = request.POST.get('email')
+        number = request.POST.get('phone')
+        desc = request.POST.get('message')
+        
+        query = Contact(name=fname, email=femail, phone=number, description=desc)
+        query.save()
+        
+        messages.info(request, 'Thanks for contacting us!')
+        
+        return redirect('contact')
+    
+    return render(request, 'contact.html')
