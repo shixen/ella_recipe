568d6b6baa0a696dc6fe816e7610e4d70c0848ef
niklas eriksson <niklaseriksson32@hotmail.com>
2024-06-02 18:27:39 +0000

    add comment and change attribute name

--- contact/models.py
+++ contact/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-# Create your models here.
 
+# model for contact form 
 class Contact(models.Model):
     name=models.CharField(max_length=30)
     email=models.EmailField()
     phone=models.CharField(max_length=12)
-    message=models.TextField(blank=True)
+    description=models.TextField(blank=True)
