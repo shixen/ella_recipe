ac8bc676b167a963d153b5dfe77ef4287ef21712
niklas eriksson <niklaseriksson32@hotmail.com>
2024-06-02 18:25:58 +0000

    add urls to urls.py to connect path

--- recipe/urls.py
+++ recipe/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('home.urls'), name="home_urls"),
+    path('contact/', include('contact.urls')),
     path('summernote/', include('django_summernote.urls')),
-    path("accounts/", include("allauth.urls")),
+    path('accounts/', include('allauth.urls')),
 ]
