2527b4f11bc5bc979b34f7e07ec974471d12c981
niklas eriksson <niklaseriksson32@hotmail.com>
2024-06-04 20:53:57 +0000

    add view to comments

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
-
+from .forms import CommentForm
 
 class PostList(generic.ListView):
     model = Post
@@ -23,5 +23,6 @@ class recipeDetails(View):
             {
                 "post": post,
                 "comments": comments,
+                "comment_form": CommentForm()
             }
         )
