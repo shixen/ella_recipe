05d835b1edbfa13452389a307b7a5132a63e97ef
niklas eriksson <niklaseriksson32@hotmail.com>
2024-06-05 21:36:37 +0000

    add url path to crud comments

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,11 @@
 from . import views
 from django.urls import path
+from .views import PostList, RecipeDetails, CommentEdit, CommentDelete
 
 # urls paths
 urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
-    path('<slug:slug>/', views.recipeDetails.as_view(), name='recipe_details'),
+    path('<slug:slug>/', views.RecipeDetails.as_view(), name='recipe_detail'),
+    path('recipe/<slug:slug>/comment/<int:comment_id>/edit/', CommentEdit, name='comment_edit'),
+    path('recipe/<slug:slug>/comment/<int:comment_id>/delete/', CommentDelete, name='comment_delete'),
 ]
