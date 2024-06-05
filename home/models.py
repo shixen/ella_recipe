05e1276855a1c448befac152616d9b7c61281a63
niklas eriksson <niklaseriksson32@hotmail.com>
2024-06-05 21:40:12 +0000

    add null True to author model

--- home/models.py
+++ home/models.py
@@ -35,7 +35,7 @@ class Comment(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="comments_author")
+        User, on_delete=models.CASCADE, related_name="comments_author", null=True)
 
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
