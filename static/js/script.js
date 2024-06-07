4a3ca39c53c738c843f31e7366b2330ac810dee5
niklas eriksson <niklaseriksson32@hotmail.com>
2024-06-07 14:06:20 +0000

    fixed type error on eventlistener

--- static/js/script.js
+++ static/js/script.js
@@ -5,12 +5,16 @@ document.addEventListener("DOMContentLoaded", function() {
     var contactForm = document.getElementById("contactform");
     var successAlert = document.getElementById("successAlert");
 
-    contactForm.addEventListener("submit", function(event) {
-        event.preventDefault();
+    if (contactForm && successAlert) {
+        contactForm.addEventListener("submit", function(event) {
+            event.preventDefault();
 
-        if (contactForm.checkValidity()) {
-            successAlert.style.display = "block";
-            contactForm.reset();
-        }
-    });
+            if (contactForm.checkValidity()) {
+                successAlert.style.display = "block";
+                contactForm.reset();
+            }
+        });
+    } else {
+        console.error("cant find element 'contactform' or 'successAlert' in DOM");
+    }
 });
