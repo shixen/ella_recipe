d7233b4ab73f1b094b1eddee39914b98a39e2691
niklas eriksson <niklaseriksson32@hotmail.com>
2024-06-07 13:01:42 +0000

    debug set true

--- recipe/settings.py
+++ recipe/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = True
 
 APPEND_SLASH=False
 
